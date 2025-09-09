98d982bbff5ac330611250f1be9a4f59ba40f911
alves1503 <henriquefdalves15@gmail.com>
2025-09-09 02:21:45 -0300

    Alteração diretorio

---
+++ udemy/curso-csharp/MySolotion/MyConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace MyConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+          
+             Multiplos comentario
+
+             */
+
+            // Este é um comentario em C#
+            #region
+            Console.WriteLine("Ola mundo!");
+            #endregion
+
+        }
+    }
+}
