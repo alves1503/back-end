ad27e2db53a7d0c9751c6771d0abf2842e2d32cf
alves1503 <henriquefdalves15@gmail.com>
2025-09-09 03:34:55 -0300

    Iniciei os estudos de entrada e saida de dados em csharp

---
+++ udemy/curso-csharp/entradaSaida/entradaSaida/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace entradaSaida
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Seja bem vindo!");
+            Console.ReadKey();
+            Console.Write("Digite seu nome:");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine($"Seja bem vindo {nome}!");
+
+        }
+    }
+}
