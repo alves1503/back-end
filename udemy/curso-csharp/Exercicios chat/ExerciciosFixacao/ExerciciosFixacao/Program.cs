31794b23b1b9d4caaaeed2fd6b62ac31b810e272
alves1503 <henriquefdalves15@gmail.com>
2025-09-17 00:36:16 -0300

    Fiz um ex de fixacao, csharp

---
+++ udemy/curso-csharp/Exercicios chat/ExerciciosFixacao/ExerciciosFixacao/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosFixacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //            Crie um programa em C# que:
+
+            //Peça ao usuário o nome, o peso(kg) e a altura(m).
+
+            //Calcule o IMC = peso / (altura * altura).
+
+            //Mostre na tela o nome da pessoa, o valor do IMC e a classificação:
+
+            //            Abaixo do peso: IMC < 18,5
+
+            //Peso normal: 18,5 ≤ IMC < 25
+
+            //Sobrepeso: 25 ≤ IMC < 30
+
+            //Obesidade: IMC ≥ 30
+
+
+            Console.Write("Digite seu nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Digite seu peso (kg): ");
+            int seuPeso = int.Parse(Console.ReadLine());
+            Console.Write("Digite sua altura (m): ");
+            double altura = double.Parse(Console.ReadLine());
+
+            double seuImc = seuPeso / (altura * altura);
+            string informacao;
+
+            if(seuImc < 18.5)
+            {
+                informacao = "Abaixo do peso";
+            } else if (seuImc < 25)
+            {
+                informacao = "Peso normal";
+            } else if (seuImc < 30)
+            {
+                informacao = "Sobrepeso";
+            } else
+            {
+                informacao = "Obesidade";
+            }
+
+            Console.WriteLine("---------------------------------------------------------------");
+            Console.WriteLine($"Seja bem vindo {nome}, sua altura: {altura}, seu peso: {seuPeso} \n" +
+                $"Você possui um IMC: {Math.Round(seuImc,2)}, considerado: {informacao}");
+
+
+        }
+    }
+}
---
+++ udemy/curso-csharp/Exercicios chat/ExerciciosFixacao/ExerciciosFixacao/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ExerciciosFixacao")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ExerciciosFixacao")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1a6d3247-394e-432b-94e4-aec5e6a0800d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
