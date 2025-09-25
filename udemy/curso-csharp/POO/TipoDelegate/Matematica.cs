4b66c248a706d0473c0f3922423f2f78af07f077
alves1503 <henriquefdalves15@gmail.com>
2025-09-24 21:05:27 -0300

    Adicionei os estudos de delgate em csharp

---
+++ udemy/curso-csharp/POO/TipoDelegate/Matematica.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipoDelegate
+{
+    internal class Matematica
+    {
+
+        public void Soma (int x, int y)
+        {
+            Console.WriteLine($"O valor de {x} + {y} = {x + y}");
+        }
+
+        public void Subtracao(int x, int y)
+        {
+            Console.WriteLine($"O valor de {x} - {y} = {x - y}");
+        }
+
+        public void Multiplicar(int x, int y)
+        {
+            Console.WriteLine($"O valor de {x} X {y} = {x * y}");
+        }
+
+        public void Divisao(int x, int y)
+        {
+            Console.WriteLine($"O valor de {x} / {y} = {x / y}");
+        }
+
+
+    }
+}
---
+++ udemy/curso-csharp/POO/TipoDelegate/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipoDelegate
+{
+    internal class Program
+    {
+        delegate void Operacao(int num1, int num2);
+        static void Main(string[] args)
+        {
+
+            Matematica m = new Matematica();
+
+            Operacao conta = null;
+
+            conta += m.Divisao();
+
+            conta(10, 20);
+
+            Console.ReadKey();
+             
+             
+
+        }
+    }
+}
