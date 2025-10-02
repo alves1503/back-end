9334a490e37f785f858e5ec85520ac2ee60106a9
alves1503 <henriquefdalves15@gmail.com>
2025-10-02 00:03:08 -0300

    Terminei os estudos de POO em  csharp

---
+++ udemy/curso-csharp/POO/Modifiicadores/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modifiicadores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Teste t = new Teste();
+            //t.nome = "Henrique";
+            t.sobrenome = "Alves";
+
+            //Console.WriteLine(t.nome);
+            Console.WriteLine(t.sobrenome);
+        }
+    }
+}
---
+++ udemy/curso-csharp/POO/Polimorfismo/Classes.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo
+{
+
+
+    public class Forma
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Altura { get; set; }
+        public int Largura { get; set; }
+        public int Raio { get; set; }
+
+
+
+        public virtual void Desenhar()
+        {
+            Console.WriteLine("Preparando para desenhar");
+        }
+
+        public virtual void Area()
+        {
+           
+        }
+
+
+
+
+
+
+
+    }
+
+
+
+
+    class Criculo : Forma
+    {
+
+
+        public override void Desenhar()
+        {
+            base.Desenhar();
+            Console.WriteLine("Desenhou um circulo");
+        }
+
+        public override void Area()
+        {
+            double area = 3.14 * (Raio * Raio);
+            Console.WriteLine($"Area do circulo: {area}");
+        }
+
+
+
+    }
+
+
+}
