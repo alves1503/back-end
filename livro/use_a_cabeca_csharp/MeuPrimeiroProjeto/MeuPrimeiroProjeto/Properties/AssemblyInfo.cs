982685ef96855e37a39b5a2ffdf7b9018002bb3d
alves1503 <henriquefdalves15@gmail.com>
2025-10-07 20:13:14 -0300

    att

---
+++ livro/use_a_cabeca_csharp/MeuPrimeiroProjeto/MeuPrimeiroProjeto/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MeuPrimeiroProjeto")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MeuPrimeiroProjeto")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6487d209-c7eb-4cfd-aa0b-6b64eb3b5df4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ puc_minas/csharp/Orm_Exemplo/Orm_Exemplo/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Orm_Exemplo
+{
+
+    public class Genero
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Decricao { get; set; }
+
+        public ICollection<Filme> Filme { get; set; }
+
+    }
+
+    public class Filme
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public int GeneroId { get; set; }
+        [ForeignKey("GeneroId)"]
+        public Genero Genero { get; set; }
+    }
+
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Genero> Genero { get; set; }
+        public DbSet<Filme> Filme { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=./SQLEXpress;Database=orm;Trusted_Connection=True;");
+        }
+    }
+
+}
+
+
+
+
+
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+}
