80ac39ec015f58354617b4e36da423a6af954949
alves1503 <henriquefdalves15@gmail.com>
2025-10-20 00:44:10 -0300

    Comecei os estudos de java

--- wex/C#/ExemploExplorando/obj/Debug/net8.0/ExemploExplorando.AssemblyInfo.cs
+++ wex/C#/ExemploExplorando/obj/Debug/net8.0/ExemploExplorando.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+717577fa56e16b45d19743cb37aa7376c1b810dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+384c37f539afea9be713898455c287732a1f196d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
