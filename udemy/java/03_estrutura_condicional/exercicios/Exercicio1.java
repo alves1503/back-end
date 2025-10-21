fa8f6d451a73db6703452099d8c1e19c1e22ffdc
alves1503 <henriquefdalves15@gmail.com>
2025-10-21 19:15:06 -0300

    Terminei o exercicio1 de veridicar se o numero e negativo em java

--- udemy/java/03_estrutura_condicional/exercicios/Exercicio1.java
+++ udemy/java/03_estrutura_condicional/exercicios/Exercicio1.java
@@ -1,8 +1,24 @@
+
+import java.util.Scanner;
+import javax.swing.event.SwingPropertyChangeSupport;
+
 public class Exercicio1 {
 
-    
+    public static void main(String[] args) {
 
+        int numero;
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("Digite um número: ");
+        numero = sc.nextInt();
 
+        if(numero < 0) {
+            System.out.println("O numero " + numero + " é negativo");
+        } else {
+            System.out.println("O numero " + numero + " é positivo");
+
+        }
+
+    }
 
 }
