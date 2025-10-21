3b6e8a359941047caa8d9c151d2308fafca4f869
alves1503 <henriquefdalves15@gmail.com>
2025-10-21 19:22:21 -0300

    Terminei o exercicio2 de verificar se o numero é par ou impar em java

--- udemy/java/03_estrutura_condicional/exercicios/Exercicio2.java
+++ udemy/java/03_estrutura_condicional/exercicios/Exercicio2.java
@@ -1,12 +1,21 @@
-public class Exercicio2 {
 
+import java.util.Scanner;
+
+public class Exercicio2 {
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        int numero;
 
+        System.out.print("Digite um número: ");
+        numero = sc.nextInt();
 
-
-
+        if (numero % 2 == 0) {
+            System.out.println("Numero: " + numero + " = Par");
+        } else {
+            System.out.println("Numero: " + numero + " = Impar");
+        }
 
     }
 
